ca8bdf976794ab264c72f8db36c2b93c0a176d3c
Aseel-Dweedar <archaseel.1992@gmail.com>
2021-06-18 23:10:56 +0300

    add cache memory

--- controller/movies.controller.js
+++ controller/movies.controller.js
@@ -1,23 +1,32 @@
-const axios = require("axios");
 require("dotenv").config();
+const axios = require("axios");
+const Cache = require("../helper/cache");
+const cacheObj = new Cache();
 
 const Movies = require("../models/Movies.model");
 const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 
 const moviesController = (req, res) => {
     const city = req.query.city;
+    const requestKey = `movies-${city}`;
     if (city) {
-        const moviesUrl = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${city}`;
-        axios
-            .get(moviesUrl)
-            .then((response) => {
-                console.log(response);
-                const responseData = response.data.results.map((movie) => new Movies(movie));
-                res.json(responseData);
-            })
-            .catch((error) => {
-                res.send(error.message);
-            });
+        if (cacheObj[requestKey] && Date.now() - cacheObj[requestKey].timeStamp < 86400000) {
+            res.json(cacheObj[requestKey].data);
+        } else {
+            const moviesUrl = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${city}`;
+            axios
+                .get(moviesUrl)
+                .then((axiosResponse) => {
+                    const arrayOfMovies = axiosResponse.data.results.map((movie) => new Movies(movie));
+                    cacheObj[requestKey] = {};
+                    cacheObj[requestKey].data = arrayOfMovies;
+                    cacheObj[requestKey].timeStamp = Date.now();
+                    res.json(arrayOfMovies);
+                })
+                .catch((error) => {
+                    res.send(error.message);
+                });
+        }
     } else {
         res.send("please provide the City name");
     }
